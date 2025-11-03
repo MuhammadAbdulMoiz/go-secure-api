0be55dd38a510c1cdea2cb5c6f25462d2db362cf
Muhammad Abdul Moiz <moiza354@gmail.com>
2025-11-03 14:24:08 +0500

    implementing getUser logic

--- controllers/userController.go
+++ controllers/userController.go
@@ -1,10 +1,16 @@
 package controllers
 
 import (
+	"context"
 	"go-secure-api/database"
+	"go-secure-api/helpers"
+	"go-secure-api/models"
+	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -28,7 +34,24 @@ func GetUsers() gin.HandlerFunc {
 }
 
 func GetUser() gin.HandlerFunc {
-	return func(c *gin.Context) {}
+	return func(c *gin.Context) {
+		userId := c.Param("userID")
+		if err := helpers.MatchUserTypeToUid(c, userId); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": error.Error(err)})
+			return
+		}
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		var user models.User
+		err := userCollection.FindOne(ctx, bson.M{"userID": userId}).Decode(&user)
+		defer cancel()
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": error.Error(err)})
+			return
+		}
+		c.JSON(http.StatusOK, user)
+
+	}
 }
 
 func hashPassword(password string) string {
--- helpers/authHelper.go
+++ helpers/authHelper.go
@@ -1,1 +1,32 @@
 package helpers
+
+import (
+	"errors"
+
+	"github.com/gin-gonic/gin"
+)
+
+func MatchUserTypeToUid(c *gin.Context, userId string) (err error) {
+	userType := c.GetString("user_type")
+	uid := c.GetString("uid")
+	err = nil
+
+	if userType != "ADMIN" && uid != userId {
+		return errors.New("unauthorized access")
+	}
+
+	err = CheckUserType(c, userType)
+	return err
+
+}
+
+func CheckUserType(c *gin.Context, role string) (err error) {
+	userType := c.GetString("user_type")
+	err = nil
+
+	if userType != role {
+		return errors.New("unauthorized access")
+	}
+
+	return err
+}
